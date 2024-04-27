02e8087949e0dcc8d81d7ac683ca47384ba57160
Reinhard <octave-user@a1.net>
2024-04-27 18:24:32 +0200

    Disable complex conjugate solve

--- src/pastix.cc
+++ src/pastix.cc
@@ -809,13 +809,6 @@ octave_value_list PastixObject<T>::eval(const octave_value_list& args, int nargo
           case 0:
                sys = PastixNoTrans;
                break;
-          case 1:
-               if constexpr(std::is_same<T, double>::value) {
-                    sys = PastixTrans;
-               } else {
-                    sys = PastixConjTrans;
-               }
-               break;
           case 2:
                sys = PastixTrans;
                break;
