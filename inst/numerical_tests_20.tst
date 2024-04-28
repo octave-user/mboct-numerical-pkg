1feaf749cf1a5f4ed5f081daeba8a99de5c3040b
Reinhard <octave-user@a1.net>
2024-04-28 11:21:38 +0200

    Add support for complex matrices

--- inst/numerical_tests_20.tst
+++ inst/numerical_tests_20.tst
@@ -1,35 +1,47 @@
 ## numerical_tests.tst:20
 %!test
-%!  if (isempty(which("sp_sym_mtimes")))
-%!    warning("sp_sym_mtimes was not installed");
-%!    return
-%!  endif
-%!   state = rand("state");
-%!   unwind_protect
-%!     rand("seed", 0);
-%!     tol = eps^0.9;
-%!     for i=1:10
-%!       for N=[1, 10, 100, 1000]
-%!         for M=[1, 10, 20, 100]
-%!           A = sprand(N, N, 0.01);
-%!           A += A.';
-%!           x = rand(N, M);
-%!           b = A * x;
-%!           [r, c, d] = find(A);
-%!           for k=1:2
+%! if (isempty(which("sp_sym_mtimes")))
+%!   warning("sp_sym_mtimes was not installed");
+%!   return
+%! endif
+%! state = rand("state");
+%! unwind_protect
+%!   rand("seed", 0);
+%!   tol = eps^0.9;
+%!   for k=1:2
+%!     for l=1:2
+%!       for i=1:10
+%!         for N=[1, 10, 100, 1000]
+%!           for M=[1, 10, 20, 100]
+%!             A = sprand(N, N, 0.01);
 %!             switch (k)
-%!               case 1
-%!                 idx = find(r >= c);
 %!               case 2
-%!                 idx = find(r <= c);
+%!                 A += 1j * sprand(N, N, 0.01);
 %!             endswitch
-%!             A2 = sparse(r(idx), c(idx), d(idx), N, N);
-%!             b2 = sp_sym_mtimes(A2, x);
-%!             assert(norm(b2 - b) <= tol * norm(b));
+%!             A += A.';
+%!             x = rand(N, M);
+%!             switch (l)
+%!               case 2
+%!                 x += 1j * rand(N, M);
+%!             endswitch
+%!             b = A * x;
+%!             [r, c, d] = find(A);
+%!             for k=1:2
+%!               switch (k)
+%!                 case 1
+%!                   idx = find(r >= c);
+%!                 case 2
+%!                   idx = find(r <= c);
+%!               endswitch
+%!               A2 = sparse(r(idx), c(idx), d(idx), N, N);
+%!               b2 = sp_sym_mtimes(A2, x);
+%!               assert(norm(b2 - b) <= tol * norm(b));
+%!             endfor
 %!           endfor
 %!         endfor
 %!       endfor
 %!     endfor
-%!   unwind_protect_cleanup
-%!     rand("state", state);
-%!   end_unwind_protect
+%!   endfor
+%! unwind_protect_cleanup
+%!   rand("state", state);
+%! end_unwind_protect
--- src/sp_sym_mtimes.cc
+++ src/sp_sym_mtimes.cc
@@ -1,4 +1,4 @@
-// Copyright (C) 2018(-2021) Reinhard <octave-user@a1.net>
+// Copyright (C) 2018(-2024) Reinhard <octave-user@a1.net>
 
 // This program is free software; you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
@@ -20,13 +20,106 @@
 // PKG_ADD: autoload ("sp_sym_mtimes", "__mboct_numerical__.oct");
 // PKG_DEL: autoload ("sp_sym_mtimes", "__mboct_numerical__.oct", "remove");
 
+template <typename T>
+struct MatrixTypeTraits;
+
+template <>
+struct MatrixTypeTraits<double>
+{
+     typedef SparseMatrix SparseMatrixType;
+     typedef Matrix DenseMatrixType;
+
+     static SparseMatrixType sparse_matrix_value(const octave_value& ov) {
+          assert(ov.isreal());
+          return ov.sparse_matrix_value();
+     }
+
+     static DenseMatrixType matrix_value(const octave_value& ov) {
+          assert(ov.isreal());
+          return ov.matrix_value();
+     }
+};
+
+template <>
+struct MatrixTypeTraits<std::complex<double>>
+{
+     typedef SparseComplexMatrix SparseMatrixType;
+     typedef ComplexMatrix DenseMatrixType;
+
+     static SparseMatrixType sparse_matrix_value(const octave_value& ov) {
+          assert(ov.iscomplex());
+          return ov.sparse_complex_matrix_value();
+     }
+
+     static DenseMatrixType matrix_value(const octave_value& ov) {
+          assert(ov.iscomplex());
+          return ov.complex_matrix_value();
+     }
+};
+
+template <typename TA, typename TX>
+struct MatrixProductResultType;
+
+template <>
+struct MatrixProductResultType<double, double> {
+     typedef double type;
+};
+
+template <>
+struct MatrixProductResultType<std::complex<double>, std::complex<double>> {
+     typedef std::complex<double> type;
+};
+
+template <>
+struct MatrixProductResultType<std::complex<double>, double> {
+     typedef std::complex<double> type;
+};
+
+template <>
+struct MatrixProductResultType<double, std::complex<double>> {
+     typedef std::complex<double> type;
+};
+
+template <typename TA, typename TX>
+void sp_sym_mtimes(const octave_value_list& args, octave_value_list& retval)
+{
+     typedef typename MatrixProductResultType<TA, TX>::type T;
+     const auto A = MatrixTypeTraits<TA>::sparse_matrix_value(args(0));
+     const auto x = MatrixTypeTraits<TX>::matrix_value(args(1));
+
+     const octave_idx_type* const Aridx = A.ridx();
+     const octave_idx_type* const Acidx = A.cidx();
+     const auto* const Adata = A.data();
+
+     typename MatrixTypeTraits<T>::DenseMatrixType b(A.rows(), x.columns(), 0.);
+
+     for (octave_idx_type l = 0; l < x.columns(); ++l) {
+          for (octave_idx_type j = 0; j < A.columns(); ++j) {
+               for (octave_idx_type k = Acidx[j]; k < Acidx[j + 1]; ++k) {
+                    const octave_idx_type i = Aridx[k];
+
+                    const auto Aij = Adata[k];
+
+                    b.xelem(i, l) += Aij * x.xelem(j, l);
+
+                    if (i != j) {
+                         b.xelem(j, l) += Aij * x.xelem(i, l);
+                    }
+
+               }
+          }
+     }
+
+     retval.append(b);
+}
+
 DEFUN_DLD (sp_sym_mtimes, args, nargout,
            "-*- texinfo -*-\n"
            "@deftypefn {} @var{b} = sp_sym_mtimes(@var{A},  @var{x})\n\n"
            "Compute the product @var{b} = (@var{A} + @var{A}.' - diag(diag(@var{A})))  * @var{x}\n\n"
-	   "@var{A} @dots{} upper- or lower-triangular part of a real symmetric sparse matrix\n\n"
-	   "@var{x} @dots{} a real dense matrix\n\n"
-	   "@var{b} @dots{} the matrix - matrix product\n"
+           "@var{A} @dots{} upper- or lower-triangular part of a real symmetric sparse matrix\n\n"
+           "@var{x} @dots{} a real dense matrix\n\n"
+           "@var{b} @dots{} the matrix - matrix product\n"
            "@end deftypefn\n")
 {
         octave_value_list retval;
@@ -51,33 +144,15 @@ DEFUN_DLD (sp_sym_mtimes, args, nargout,
                 return retval;
         }
 
-        const SparseMatrix A = args(0).sparse_matrix_value();
-        const Matrix x = args(1).matrix_value();
-
-        const octave_idx_type* const Aridx = A.ridx();
-        const octave_idx_type* const Acidx = A.cidx();
-        const double* const Adata = A.data();
-
-        Matrix b(A.rows(), x.columns(), 0.);
-
-        for (octave_idx_type l = 0; l < x.columns(); ++l) {
-                for (octave_idx_type j = 0; j < A.columns(); ++j) {
-                        for (octave_idx_type k = Acidx[j]; k < Acidx[j + 1]; ++k) {
-                                const octave_idx_type i = Aridx[k];
-
-                                const double Aij = Adata[k];
-
-                                b.xelem(i, l) += Aij * x.xelem(j, l);
-
-                                if (i != j) {
-                                        b.xelem(j, l) += Aij * x.xelem(i, l);
-                                }
-
-                        }
-                }
+        if (args(0).isreal() && args(1).isreal()) {
+             sp_sym_mtimes<double, double>(args, retval);
+        } else if (args(0).isreal() && args(1).iscomplex()) {
+             sp_sym_mtimes<double, std::complex<double>>(args, retval);
+        } else if (args(0).iscomplex() && args(1).isreal()) {
+             sp_sym_mtimes<std::complex<double>, double>(args, retval);
+        } else {
+             sp_sym_mtimes<std::complex<double>, std::complex<double>>(args, retval);
         }
-
-        retval.append(b);
 
         return retval;
 }
