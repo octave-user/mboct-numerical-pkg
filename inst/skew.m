702be136c1053b102e24a9d7d3634fb1a4645d0b
Reinhard <octave-user@a1.net>
2025-08-24 10:30:08 +0200

    Move all tests to .tst files

--- inst/skew.m
+++ inst/skew.m
@@ -22,33 +22,8 @@ function A = skew(a)
   if (~(nargin == 1 && isvector(a) && numel(a) == 3))
     print_usage();
   endif
-    
+
   A = [  0,     -a(3),   a(2);
          a(3),   0,     -a(1);
         -a(2),   a(1),   0     ];
 endfunction
-
-%!test
-%! try
-%! state = rand("state");
-%! unwind_protect
-%!  rand("seed", 0);
-%!  N = 1000;
-%!  a = rand(3, N);
-%!  b = rand(3, N);
-%!  c = cross(a, b);
-%!  c1 = c2 = zeros(3, N);
-%!  for i=1:N
-%!    c1(:, i) = skew(a(:, i)) * b(:, i);
-%!    c2(:, i) = -skew(b(:, i)) * a(:, i);
-%!  endfor
-%!  assert(c1, c);
-%!  assert(c2, c);
-%! unwind_protect_cleanup
-%!  rand("state", state);
-%! end_unwind_protect
-%! catch
-%!   gtest_error = lasterror();
-%!   gtest_fail(gtest_error, evalin("caller", "__file"));
-%!   rethrow(gtest_error);
-%! end_try_catch
--- src/sp_sym_mtimes.cc
+++ src/sp_sym_mtimes.cc
@@ -157,36 +157,3 @@ DEFUN_DLD (sp_sym_mtimes, args, nargout,
 
         return retval;
 }
-
-/*
-%!test
-%! state = rand("state");
-%! unwind_protect
-%!   rand("seed", 0);
-%!   tol = eps^0.9;
-%!   for i=1:10
-%!     for N=[1, 10, 100, 1000]
-%!       for M=[1, 10, 20, 100]
-%!         A = sprand(N, N, 0.01);
-%!         A += A.';
-%!         x = rand(N, M);
-%!         b = A * x;
-%!         [r, c, d] = find(A);
-%!         for k=1:2
-%!           switch (k)
-%!           case 1
-%!             idx = find(r >= c);
-%!           case 2
-%!             idx = find(r <= c);
-%!           endswitch
-%!           A2 = sparse(r(idx), c(idx), d(idx), N, N);
-%!           b2 = sp_sym_mtimes(A2, x);
-%!           assert(b2, b, tol * max(max(abs(b))));
-%!         endfor
-%!       endfor
-%!     endfor
-%!   endfor
-%! unwind_protect_cleanup
-%!   rand("state", state);
-%! end_unwind_protect
-*/
